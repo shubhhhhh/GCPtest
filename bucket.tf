3b046442b531bf3cbd80f592b3d1d985a5c106e9
shubham <shubham@gmail.com>
2025-06-29 15:26:12 +0000

    Bucket Modification

--- bucket.tf
+++ bucket.tf
@@ -2,6 +2,7 @@ resource "google_storage_bucket" "static" {
  name          = "shubhms_bucket"
  location      = "US"
  storage_class = "STANDARD"
+ force_destroy = true
 
  uniform_bucket_level_access = true
 }
